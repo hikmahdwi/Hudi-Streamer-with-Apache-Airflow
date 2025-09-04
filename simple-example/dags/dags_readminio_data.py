92b2edc7335185cebbc65dcab2e7e57f830d0a1a
Hikmah Dwiyanti Nasir <73873040+hikmahdwi@users.noreply.github.com>
2025-09-04 21:55:10 +0800

    Create dags_readminio_data.py

---
+++ simple-example/dags/dags_readminio_data.py
@@ -0,0 +1,18 @@
+import datetime
+from airflow.sdk import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+with DAG(
+  dag_id="minio_read_test",
+  start_date=datetime.datetime(2025, 7, 14),
+  schedule="@once",
+):
+  SparkSubmitOperator(
+    task_id="minio_read_test_task",
+    application="jobs/job_spark_readminio.py",
+    conn_id="spark_default",
+    packages="org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262,org.apache.hudi:hudi-spark3-bundle_2.12:0.15.0",
+    verbose=True
+  )
+
+# Note: The connection ID (conn_id) used in this DAG must be created through the Airflow interface under Admin > Connections.
